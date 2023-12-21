9dd6cd56f20f5029299cc35ea81ff7fef8b7ab75
huzaifa7399 <muhammadhuzaifasaleem@yahoo.com>
2023-12-21 13:58:22 +0500

    easy-email-editor added

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,14 @@ import "./App.css";
 import GrapeEmailEditor from "./EmailEditor/grape-email-editor";
 import grapesjs from "grapesjs";
 import { useEffect, useRef } from "react";
+import EasyEmailEditor from "./EmailEditor/easy-email-editor";
 
 function App() {
-  const emailEditorRef = useRef(null);
-
-  const getHtmlWithStyles = () => {
-    let mainDiv = document.querySelector("#gjs-mdl-c");
-    console.log(mainDiv);
-    let secondDiv = mainDiv.querySelector("div:first-child");
-    console.log(secondDiv);
-    // const editor = grapesjs.editors[0]; // Assuming only one editor is present
-
-    // if (editor) {
-    //   const htmlWithStyles = editor.getHtml();
-    //   console.log(htmlWithStyles);
-    // }
-  };
-  // useEffect(() => {
-  //   if (emailEditorRef.current && emailEditorRef.current.editor)
-  //     emailEditorRef.current?.editor.exportHtml((data) => {
-  //       //console design as html + object
-  //       const { design, html } = data;
-  //       console.log("exportHtml", html);
-  //       console.log("design", design);
-  //     });
-  // }, [emailEditorRef.current]);
-
   return (
     <div className="App">
       {/* <EmailEditor emailEditorRef={emailEditorRef} /> */}
-      <GrapeEmailEditor />
-      <button onClick={getHtmlWithStyles}>Get HTML with Styles</button>
+      {/* <GrapeEmailEditor />  */}
+      <EasyEmailEditor />
     </div>
   );
 }
