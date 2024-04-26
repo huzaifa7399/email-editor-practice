2c70cec548f92f6bba06418acbc0a990ded93545
huzaifa7399 <muhammadhuzaifasaleem@yahoo.com>
2024-04-26 15:34:13 +0500

    package removed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import GrapeEmailEditor from "./EmailEditor/grape-email-editor";
 import HomePage from "./pages";
-import EasyEmailEditor from "./EmailEditor/easy-email-editor";
 import EmailEditorPreview from "./EmailEditor/react-email-editor";
 import { useRef } from "react";
 
@@ -13,7 +12,6 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/grape-editor" element={<GrapeEmailEditor />} />
-        <Route path="/easy-editor" element={<EasyEmailEditor />} />
         <Route
           path="/unlayer-editor"
           element={<EmailEditorPreview emailEditorRef={emailEditorRef} />}
